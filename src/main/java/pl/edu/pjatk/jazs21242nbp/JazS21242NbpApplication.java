e8cb274e8b2bfacc578c2879374fd4a3cf23d49b
s21242-PJ <s21242@pjwstk.edu.pl>
2021-07-04 19:31:05 +0200

    dependencies + property added

---
+++ src/main/java/pl/edu/pjatk/jazs21242nbp/JazS21242NbpApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pjatk.jazs21242nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS21242NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS21242NbpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/pjatk/jazs21242nbp/JazS21242NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pjatk.jazs21242nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS21242NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
