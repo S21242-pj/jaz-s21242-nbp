643c938229c765cb44e88c9a34eb9864ea721803
s21242-PJ <s21242@pjwstk.edu.pl>
2021-07-04 19:36:40 +0200

    Swagger + RestTemplate

---
+++ src/main/java/pl/edu/pjatk/jazs21242nbp/config/SpringFoxConfig.java
@@ -0,0 +1,39 @@
+package pl.edu.pjatk.jazs21242nbp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+@Configuration
+@EnableSwagger2
+public class SpringFoxConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .paths(PathSelectors.any())
+                .build().apiInfo(info());
+    }
+
+    private ApiInfo info() {
+        return new ApiInfo(
+                "get average price from NBP",
+                "get currency price form NBP API",
+                "1.0",
+                null,
+                new Contact("Szymon Dejewski ", null, "s21242@pjwstk.edu.pl"),
+                null,
+                null,
+                Collections.emptyList());
+    }
+}
