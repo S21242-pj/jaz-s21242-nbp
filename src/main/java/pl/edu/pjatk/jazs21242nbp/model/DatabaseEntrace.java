885f38faa112e79a6d6f56be5842c305fd564a7e
s21242-PJ <s21242@pjwstk.edu.pl>
2021-07-04 20:21:51 +0200

    added some in DBentrace

--- src/main/java/pl/edu/pjatk/jazs21242nbp/model/DatabaseEntrace.java
+++ src/main/java/pl/edu/pjatk/jazs21242nbp/model/DatabaseEntrace.java
@@ -1,4 +1,28 @@
 package pl.edu.pjatk.jazs21242nbp.model;
 
+import io.swagger.annotations.ApiModelProperty;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class DatabaseEntrace {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @ApiModelProperty(notes = "Currenct ID")
+    private Long id;
+
+
+
+
+
+    public DatabaseEntrace(){
+    }
+
+
+
+
 }
