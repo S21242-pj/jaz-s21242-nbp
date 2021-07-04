f651bad618dc72e1774d9b46dd97e55efd0f258d
s21242-PJ <s21242@pjwstk.edu.pl>
2021-07-04 20:07:37 +0200

    addes service + controller

--- src/main/java/pl/edu/pjatk/jazs21242nbp/controller/ControllerNbp.java
+++ src/main/java/pl/edu/pjatk/jazs21242nbp/controller/ControllerNbp.java
@@ -1,4 +1,19 @@
 package pl.edu.pjatk.jazs21242nbp.controller;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.edu.pjatk.jazs21242nbp.service.ServiceNbp;
+
+@RestController
+@RequestMapping("/bank")
 public class ControllerNbp {
+
+    private final ServiceNbp serviceNbp;
+
+    public ControllerNbp(ServiceNbp serviceNbp) {
+        this.serviceNbp = serviceNbp;
+    }
+
+
 }
--- src/main/java/pl/edu/pjatk/jazs21242nbp/service/ServiceNbp.java
+++ src/main/java/pl/edu/pjatk/jazs21242nbp/service/ServiceNbp.java
@@ -1,4 +1,17 @@
 package pl.edu.pjatk.jazs21242nbp.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import pl.edu.pjatk.jazs21242nbp.repository.RepositoryNbp;
+
+@Service
 public class ServiceNbp {
+
+    private final RestTemplate restTemplate;
+    private final RepositoryNbp repositoryNbp;
+
+    public ServiceNbp(RestTemplate restTemplate, RepositoryNbp repositoryNbp) {
+        this.restTemplate = restTemplate;
+        this.repositoryNbp = repositoryNbp;
+    }
 }
