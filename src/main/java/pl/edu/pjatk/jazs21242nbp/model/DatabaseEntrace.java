63c28177b3ffc92b94cd1b229cc263deb387919b
s21242-PJ <s21242@pjwstk.edu.pl>
2021-07-04 20:59:16 +0200

    addres db entrace

--- src/main/java/pl/edu/pjatk/jazs21242nbp/model/DatabaseEntrace.java
+++ src/main/java/pl/edu/pjatk/jazs21242nbp/model/DatabaseEntrace.java
@@ -6,16 +6,55 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 @Entity
 public class DatabaseEntrace {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @ApiModelProperty(notes = "Currenct ID")
+    @ApiModelProperty(notes = "Currency ID")
     private Long id;
 
-    public DatabaseEntrace(){
+    @ApiModelProperty(notes = "Currency name", required = true, dataType = "String")
+    private String currency;
+
+    @ApiModelProperty(notes = "First day from range", required = true, dataType = "String")
+    private String firstDate;
+
+    @ApiModelProperty(notes = "Last day from range", required = true, dataType = "String")
+    private String lastDate;
+
+    @ApiModelProperty(notes = "Average prise from number of days", required = true, dataType = "Double")
+    private Double avgPrices;
+
+    @ApiModelProperty(notes = "Date and hour when average price was taken", required = true, dataType = "String")
+    private final String getTdDay = getTdDay();
+
+    private String getTdDay() {
+        Date date = Calendar.getInstance().getTime();
+        DateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd");
+        return dateFormat.format(date);
     }
+
+    public DatabaseEntrace(String currency, String firstDate, String lastDate, Double avgPrices) {
+        this.currency = currency;
+        this.firstDate = firstDate;
+        this.lastDate = lastDate;
+        this.avgPrices = avgPrices;
+    }
+
+    public DatabaseEntrace(String currency, String firstDate, String lastDate, double avgPrices) {
+
+
+    }
+
+
+    public DatabaseEntrace() {
+    }
+
 
 }
